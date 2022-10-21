9a4e85e357f2788ae0cc8345d5f357be2b1d758c
leikoe <chachafrdark@gmail.com>
2022-10-22 00:46:29 +0200

    added mightContain benchmark

--- src/test/java/com/leikoe/Bencher.java
+++ src/test/java/com/leikoe/Bencher.java
@@ -1,11 +1,35 @@
 package com.leikoe;
 
+import com.leikoe.hash.MurmurHash2;
 import org.openjdk.jmh.annotations.*;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
+import java.util.function.ToIntFunction;
 
 @State(Scope.Benchmark)
 public class Bencher {
+
+    BloomFilter<Integer> arrayListBloomFilter = Bencher.makeExampleArrayListBloomFilter(500, values);
+    static int[] values = {12, 372, 3972};
+    static int[] testValues = {232, 193, 22, 12, 372, 90, 3972};
+
+    public static BloomFilter<Integer> makeExampleArrayListBloomFilter(int capacity, int[] values) {
+        List<Boolean> boolArrayList = new ArrayList<>(capacity);
+        for (int i=0; i<500; i++)
+            boolArrayList.add(false);
+
+        List<ToIntFunction<Integer>> hashFunctions = new ArrayList<>();
+        hashFunctions.add(new MurmurHash2<Integer>(23792387));
+
+        BloomFilter<Integer> arrayListBloomFilter = new BloomFilter<>(boolArrayList, hashFunctions);
+        for (int i: values) {
+            arrayListBloomFilter.add(i);
+        }
+        return arrayListBloomFilter;
+    }
 
     public static void main(String[] args) throws Exception {
         org.openjdk.jmh.Main.main(args);
@@ -15,9 +39,8 @@ public class Bencher {
     @BenchmarkMode(Mode.AverageTime)
     @OutputTimeUnit(TimeUnit.NANOSECONDS)
     public void measureArrayListBloomFilterContains(org.openjdk.jmh.infra.Blackhole bh) {
-        int a = 1;
-        int b = 2;
-        int c = a + b;
-        bh.consume(c);
+        for (int i: values) {
+            bh.consume(arrayListBloomFilter.mightContain(i));
+        }
     }
 }
