9aa82c73a2139b4adb15c12a9db8b6e1c193256c
leikoe <chachafrdark@gmail.com>
2022-10-22 00:46:52 +0200

    added tests

---
+++ src/test/java/com/leikoe/BloomFilterTest.java
@@ -0,0 +1,37 @@
+package com.leikoe;
+
+import static org.junit.Assert.*;
+
+public class BloomFilterTest {
+
+    BloomFilter<Integer> arrayListBloomFilter;
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+        arrayListBloomFilter = Bencher.makeExampleArrayListBloomFilter(500, new int[]{12, 87, 43, 22, 8, 97});
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+    }
+
+    @org.junit.Test
+    public void add() {
+    }
+
+    @org.junit.Test
+    public void mightContain() {
+        assertTrue(arrayListBloomFilter.mightContain(12));
+        assertTrue(arrayListBloomFilter.mightContain(87));
+        assertTrue(arrayListBloomFilter.mightContain(43));
+        assertTrue(arrayListBloomFilter.mightContain(22));
+        assertTrue(arrayListBloomFilter.mightContain(8));
+        assertTrue(arrayListBloomFilter.mightContain(97));
+
+//        assertFalse(arrayListBloomFilter.mightContain(67));
+//        assertFalse(arrayListBloomFilter.mightContain(7));
+//        assertFalse(arrayListBloomFilter.mightContain(748));
+//        assertFalse(arrayListBloomFilter.mightContain(23));
+//        assertFalse(arrayListBloomFilter.mightContain(328));
+    }
+}
