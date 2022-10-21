16b51aa45a2e96418bee4b41185b58a1a73c6bf9
leikoe <chachafrdark@gmail.com>
2022-10-22 00:46:05 +0200

    added MurmurHash2 hashing function

--- README.md
+++ README.md
@@ -3,8 +3,10 @@
 - https://fr.wikipedia.org/wiki/Filtre_de_Bloom
 - https://llimllib.github.io/bloomfilter-tutorial/
 - https://fr.wikipedia.org/wiki/Fonction_de_hachage
+### hash functions references:
+- https://sites.google.com/site/murmurhash/
 
-# how to run
+## how to run
 ```bash
 mvn clean compile exec:java
 ```
---
+++ src/main/java/com/leikoe/hash/MurmurHash2.java
@@ -0,0 +1,68 @@
+package com.leikoe.hash;
+
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+import java.util.function.ToIntFunction;
+
+
+public class MurmurHash2<T> implements ToIntFunction<T> {
+
+    private final int seed;
+
+    public MurmurHash2(int seed) {
+        this.seed = seed;
+    }
+
+    public int hash(T object) {
+        byte[] data = Utils.objectToBytes(object);
+
+        int m = 0x5db1e995;
+        int r = 24;
+
+        int data_len = data.length;
+        int h = seed ^ data_len;
+
+        int i = 0;
+        while (data_len >= 4) {
+            // load 4 bytes into a buffer
+            byte[] buffer = Arrays.copyOfRange(data, i, i+4);
+
+            // convert the byte[4] buffer to an int
+            int k = ByteBuffer.wrap(buffer).getInt();
+
+            k *= m;
+            k ^= k >> r;
+            k *= m;
+
+            h *= m;
+            h ^= k;
+
+            i += 4;
+            data_len -= 4;
+        }
+
+        // Handle the last few bytes of the input array
+
+        switch(data_len)
+        {
+            case 3: h ^= data[2] << 16;
+            case 2: h ^= data[1] << 8;
+            case 1: h ^= data[0];
+                h *= m;
+        };
+
+        // Do a few final mixes of the hash to ensure the last few
+        // bytes are well-incorporated.
+
+        h ^= h >> 13;
+        h *= m;
+        h ^= h >> 15;
+
+        return h;
+    }
+
+    @Override
+    public int applyAsInt(T value) {
+        return hash(value);
+    }
+}
