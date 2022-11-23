679153c37c1c0cac0a9a8a2be2378d9375821d32
Leiko Lei <chachafrdark@gmail.com>
2022-11-23 19:54:33 +0100

    docs for UFBF.java

--- src/main/java/com/leikoe/UFBF.java
+++ src/main/java/com/leikoe/UFBF.java
@@ -16,9 +16,10 @@ public class UFBF<T> extends BloomFilter<T> {
     IntVector vr_unit;
 
     /**
+     * Constructor
      * This creates a BloomFilter instance using the provided bits container
      *
-     * @param expectedInsertCount
+     * @param expectedInsertCount the expected number of add() calls
      */
     public UFBF(int expectedInsertCount) {
         super(
@@ -42,7 +43,23 @@ public class UFBF<T> extends BloomFilter<T> {
     }
 
     /**
+     * Main hash function
+     * vector implementation of the fast hash used in google guava's bloomfilter
+
      * from "Less Hashing, Same Performance: Building a Better Bloom Filter" by Adam Kirsch
+     *
+     * @param seeds IntVector of the seeds
+     * @param hash1 first hash
+     * @param hash2 second hash
+     * @return IntVector of hash1 + seed * hash2
+     */
+    private IntVector hash(IntVector seeds, int hash1, int hash2) {
+        return seeds.mul(hash2).add(hash1);
+    }
+
+    /**
+     * inserts a generic value into the filter
+     * this was implemented using the whitepaper "Ultra-Fast Bloom Filters using SIMD Techniques" by Jianyuan Lu
      *
      * @param value
      */
@@ -52,34 +69,45 @@ public class UFBF<T> extends BloomFilter<T> {
         int hash1 = (int) hash64;
         int hash2 = (int) (hash64 >>> 32);
 
+        // hash1 needs to be positive, this could be achieved by either bitwise not or abs()
         hash1 = Math.abs(hash1);
+
+        // get the block of k ints
         int[] block = bits.getBlock(hash1 % bits.blockCount());
 
         int i = 0;
         for (; i + SPECIES.length() <= upperBound; i += SPECIES.length()) {
+            // get the values of k as the seeds for hashing
             IntVector z = IntVector.fromArray(SPECIES, ks, i);
-            IntVector v_combinedHash = z.mul(hash2).add(hash1);
 
-            IntVector vr_val = v_combinedHash.abs();
+            // hash the seeds using hash1 and hash2 and get a positive result
+            IntVector vr_val = hash(z, hash1, hash2).abs();
+
+            // this is the vector equivalent of vr_a[k] = 1 << hash(k, hash1, hash2).abs()
             IntVector vr_a = vr_unit.lanewise(VectorOperators.LSHL, vr_val);
 
+            // store the hashes back into the bits block
             vr_a.intoArray(block, i);
         }
 
         // process the rest in scalar code
         for (; i<k; i++) {
             int pos = hash1 + ((i+1) * hash2);
-            if (pos < 0) {
-                pos = ~pos;
-            }
+            // same as for hash1
+            pos = Math.abs(pos);
+            // set the bit at pos in block[i] to true
             block[i] |= 1 << pos;
-//            bits.set(pos % bits.size(), true);
         }
 
-//        bits.setBlock(hash1, block); // useless
         this.n++;
     }
 
+    /**
+     * Membership check function
+     *
+     * @param value the value to check membership for
+     * @return whether the value is in the bloomfilter or not
+     */
     @Override
     public boolean mightContain(T value) {
         long hash64 = Murmur64.hash(value.hashCode());
@@ -92,10 +120,13 @@ public class UFBF<T> extends BloomFilter<T> {
 
         int i = 0;
         for (; i + SPECIES.length() <= upperBound; i += SPECIES.length()) {
+            // get the values of k as the seeds for hashing
             IntVector z = IntVector.fromArray(SPECIES, ks, i);
-            IntVector v_combinedHash = z.mul(hash2).add(hash1);
 
-            IntVector vr_val = v_combinedHash.abs();
+            // hash the seeds using hash1 and hash2 and get a positive result
+            IntVector vr_val = hash(z, hash1, hash2).abs();
+
+            // this is the vector equivalent of vr_a[k] = 1 << hash(k, hash1, hash2).abs()
             IntVector vr_a = vr_unit.lanewise(VectorOperators.LSHL, vr_val);
 
             // compare with block
@@ -103,6 +134,7 @@ public class UFBF<T> extends BloomFilter<T> {
             vr_b = vr_b.not();
 
             IntVector vr_test = vr_a.and(vr_b);
+            // if each bit set int vr_a isn't set in vr_b, it will return false
             if (vr_test.reduceLanes(VectorOperators.ADD) != 0) {
                 return false;
             }
@@ -111,9 +143,9 @@ public class UFBF<T> extends BloomFilter<T> {
         // process the rest in scalar code
         for (; i<k; i++) {
             int pos = hash1 + ((i+1) * hash2);
-            if (pos < 0) {
-                pos = ~pos;
-            }
+            // same as for hash1
+            pos = Math.abs(pos);
+            // if the bit at pos in block[i] is not set, return false
             if ((block[i] & 1 << pos) == 0) {
                 return false;
             }
