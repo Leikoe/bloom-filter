0b7536a1d52793909723085261e870cf7b29542b
Leiko Lei <chachafrdark@gmail.com>
2022-11-27 22:37:35 +0100

    cleaned up BlockBitSet

--- src/main/java/com/leikoe/bitscontainers/BlockBitSet.java
+++ src/main/java/com/leikoe/bitscontainers/BlockBitSet.java
@@ -4,7 +4,6 @@ import com.leikoe.IBitsBlocksContainer;
 
 public class BlockBitSet implements IBitsBlocksContainer {
     final int[][] data;
-    private long bitCount;
     private final int BLOCK_SIZE;
     private final int BLOCK_BIT_SIZE;
     private final int WORD_SIZE = 32;
@@ -15,22 +14,13 @@ public class BlockBitSet implements IBitsBlocksContainer {
         this.BLOCK_SIZE = blockSize;
         this.BLOCK_BIT_SIZE = WORD_SIZE * this.BLOCK_SIZE;
         this.data = new int[(int) Math.ceil(size/((float) this.BLOCK_BIT_SIZE))][blockSize];
-        this.bitCount = 0;
     }
 
-    /** Returns true if the bit changed value. */
-    boolean set(int bitIndex) {
-        if (get(bitIndex)) {
-            return false;
-        }
-
+    @Override
+    public void set(int bitIndex, boolean v) {
         int blockIndex = bitIndex / BLOCK_BIT_SIZE;
         int intIndex = (bitIndex % BLOCK_BIT_SIZE) / WORD_SIZE;
         data[blockIndex][intIndex] |= (1 << bitIndex); // from java's BitSet
-
-        // We turned the bit on, so increment bitCount.
-        bitCount += 1;
-        return true;
     }
 
     @Override
@@ -48,22 +38,10 @@ public class BlockBitSet implements IBitsBlocksContainer {
         return (long) data.length * Long.SIZE;
     }
 
-    /**
-     * Number of set bits (1s).
-     */
-    long bitCount() {
-        return bitCount;
-    }
-
 
     /** Returns the number of {@code long}s in the underlying long[]. */
     int dataLength() {
         return data.length;
-    }
-
-    @Override
-    public void set(int i, boolean b) {
-        set(i);
     }
 
     @Override
