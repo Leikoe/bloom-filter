076350c633c99f892cd6ba82ca41dfd235f1dd8b
Leiko Lei <chachafrdark@gmail.com>
2022-12-09 11:17:46 +0100

    added javadoc

--- src/main/java/com/leikoe/IBloomFilter.java
+++ src/main/java/com/leikoe/IBloomFilter.java
@@ -2,7 +2,25 @@ package com.leikoe;
 
 // TODO: javadoc
 public interface IBloomFilter<T> {
+    /**
+     * Insert a value in the filter
+     *
+     * @param value the value to add to the filter
+     */
     void add(T value);
+
+    /**
+     * Membership check function
+     *
+     * @param value the value to check membership for
+     * @return whether the value is in the filter or not
+     */
     boolean mightContain(T value);
+
+    /**
+     * Get the size of the filter
+     *
+     * @return the number of elements in the filter
+     */
     int size();
 }
